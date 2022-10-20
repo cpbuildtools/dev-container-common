3f2b332acc5bd9d7b7b26c2a76548fc6ca616c2c
Dan Erdelt <erd@conceptual.ca>
2022-10-20 18:09:14 +0000

    detection

--- src/package/impl/NpmPackage.ts
+++ src/package/impl/NpmPackage.ts
@@ -1,9 +1,13 @@
+import { existsSync } from "fs";
+import Path from "path";
 import { PackageJson } from "type-fest";
 import { Package } from "./Package";
-
 export class NpmPackage extends Package {
   public static async detect(data: PackageJson, path: string, filename: string) {
-    return true;
+    if (existsSync(Path.join(path, "package-lock.json"))) {
+      return true;
+    }
+    return false;
   }
 
   protected execPackageManager(cmd: string): Promise<number> {
